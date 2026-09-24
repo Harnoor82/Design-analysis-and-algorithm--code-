5d381b7cc2035b2b07aaa165e82c4b535339ac0a
Harnoor82 <HARNOOR.26011@stu.upes.ac.in>
2026-09-24 16:08:31 +0530

    Code in C

--- Day13.c
+++ Day13.c
@@ -1,7 +1,4 @@
-/*Question 13
-
-Given two sorted arrays nums1 and nums2 of
- size m and n respectively, return the median of the two sorted arrays.*/
+//Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
  #include <stdio.h>
 
